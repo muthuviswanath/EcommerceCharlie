6aba720115a7b14911141eaa863ab4cbbac48191
KDI-pulkit <pulkit.mittal@kongsbergdigital.com>
2022-08-22 22:40:43 +0530

    Update 2

---
+++ ProductApi/ProductApi/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProductApi.Models
+{
+    public partial class Cart
+    {
+        public int CartId { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public double CartTotal { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual User User { get; set; }
+    }
+}
