4e91579bc17731073e724b58aa6def264b8d4c1a
SumitKChoudhary <sumit.k.choudhary99@gmail.com>
2022-08-23 16:09:33 +0530

    product test

--- Frontend/src/app/components/products/products.component.ts
+++ Frontend/src/app/components/products/products.component.ts
@@ -6,17 +6,29 @@ import { ProductServices } from '../../services/product.services';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
+
 export class ProductsComponent implements OnInit {
 
   IProduct: Array<any> = [];
   productsList: IProduct[];
+
+  isBigEnough(element, index, array):any { 
+    return (element >= 10); 
+  } 
   constructor(private _productServices: ProductServices) {
 
   }
+  
+  
   ngOnInit(): void {
     this._productServices.getAllProducts().subscribe(
       res => this.productsList = res
-    );        // without subscribe will not be able to access.
+    );   
+        // without subscribe will not be able to access.
+       // var pd = this.productsList.filter(element=>element.productDescription=="Sarees");
+       // console.log("hello");
+       // console.log(this.productsList);
+
   }
 
 }
