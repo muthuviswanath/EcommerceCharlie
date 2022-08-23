766a9defd5e17c005b73c9fcc3f5ec724e7d689d
KDI-pulkit <pulkit.mittal@kongsbergdigital.com>
2022-08-23 23:18:10 +0530

    Added More Components

--- Frontend/src/app/cart-wishlist/cart-wishlist.module.ts
+++ Frontend/src/app/cart-wishlist/cart-wishlist.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CartWishlistRoutingModule } from './cart-wishlist-routing.module';
+import { CartComponent } from './components/cart/cart.component';
+import { OrderhistoryComponent } from './components/orderhistory/orderhistory.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CartComponent,
+    OrderhistoryComponent,
+    WishlistComponent
+  ],
   imports: [
     CommonModule,
     CartWishlistRoutingModule
