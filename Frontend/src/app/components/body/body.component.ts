6fb4c8b193db8f0b140afe9f8042cdd7731dc6a0
KDI-pulkit <pulkit.mittal@kongsbergdigital.com>
2022-08-23 11:00:41 +0530

    added bootstrap 5.0

--- Frontend/src/app/components/body/body.component.ts
+++ Frontend/src/app/components/body/body.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from "../../interfaces/IProduct";
-import { ProductServices } from '../../services/product.services';
 
 @Component({
   selector: 'app-body',
@@ -9,15 +7,9 @@ import { ProductServices } from '../../services/product.services';
 })
 export class BodyComponent implements OnInit {
 
-  IProduct: Array<any> = [];
-  productsList: IProduct[];
-  constructor(private _productServices: ProductServices) {
+  constructor() { }
 
-  }
   ngOnInit(): void {
-    this._productServices.getAllProducts().subscribe(
-      res => this.productsList = res
-    );        // without subscribe will not be able to access.
   }
 
 }
--- Frontend/src/app/components/cards/cards.component.ts
+++ Frontend/src/app/components/cards/cards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from "../../interfaces/IProduct";
+import { ProductServices } from '../../services/product.services';
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  IProduct: Array<any> = [];
+  productsList: IProduct[];
+  constructor(private _productServices: ProductServices) {
 
+  }
   ngOnInit(): void {
+    this._productServices.getAllProducts().subscribe(
+      res => this.productsList = res
+    );        // without subscribe will not be able to access.
   }
 
 }
