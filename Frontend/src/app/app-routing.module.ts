73c0be3b3d835e0e0501409c2ec6652af4aab9c8
TusharJainKDI <tushar.jain@kongsbergdigital.com>
2022-08-23 13:32:17 +0530

    Added Routing with Navbar

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsComponent } from './components/cards/cards.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/cards', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'cards', component: CardsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
