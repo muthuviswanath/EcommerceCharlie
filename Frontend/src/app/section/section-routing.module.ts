a7dbcab386d3dfc9b2a42bc0fb3b09ff82dab6ca
SumitKChoudhary <sumit.k.choudhary99@gmail.com>
2022-08-24 12:19:58 +0530

    added route

--- Frontend/src/app/section/section-routing.module.ts
+++ Frontend/src/app/section/section-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from '../cart-wishlist/components/cart/cart.component';
 import { ProductsComponent } from '../landing-page/components/products/products.component';
 import { TopProductsComponent } from '../landing-page/components/top-products/top-products.component';
 import { LoginComponent } from '../login-register/components/login/login.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'topproducts', component: TopProductsComponent },
   { path: 'product', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
