591de785a61323c16ecba1e362c8382deeac50bc
KDI-pulkit <pulkit.mittal@kongsbergdigital.com>
2022-08-25 09:21:59 +0530

    alert

--- Frontend/src/app/login-register/components/register/register.component.ts
+++ Frontend/src/app/login-register/components/register/register.component.ts
@@ -18,6 +18,7 @@ export class RegisterComponent implements OnInit {
 
   public submit():void{
     this._loginService.registerUser(this.model).subscribe();
+    alert('Registration Successful')
   }
 
 }
